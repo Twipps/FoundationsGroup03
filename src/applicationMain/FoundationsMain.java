6e02679505adb6d65a7abff390e151343966f6ed
Twipps <jsuchovic@gmail.com>
2026-05-24 15:32:00 -0700

    Test

--- src/applicationMain/FoundationsMain.java
+++ src/applicationMain/FoundationsMain.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 
-// git test
+// git test test
 
 /*******
  * <p> Title: FoundationsMain Class </p>
